c1fd1e936dce4557c9bfee845ff54672251ee307
Matt Stein <matt@pixelandtonic.com>
2021-07-22 21:17:10 -0500

    Format.

--- src/web/assets/paystack/dist/js/paymentForm.js
+++ src/web/assets/paystack/dist/js/paymentForm.js
@@ -1,10 +1,10 @@
 function initCheckout() {
-    // Because this might get executed before Paystack is loaded.
-    if (typeof Paystack === "undefined") {
-        setTimeout(initCheckout, 200);
-    } else {
-      console.log('Paystack JS loaded');
-    }
+  // Because this might get executed before Paystack is loaded.
+  if (typeof Paystack === "undefined") {
+    setTimeout(initCheckout, 200);
+  } else {
+    console.log('Paystack JS loaded');
+  }
 }
 
 initCheckout();
